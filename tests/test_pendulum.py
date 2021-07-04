cd52629d8ebcb9c6f69fe8221e3b33ec590337f5
Iced-Tea3 <83535735+Iced-Tea3@users.noreply.github.com>
2021-07-04 22:44:01 +0800

    pendulum test

--- tests/test_pendulum.py
+++ tests/test_pendulum.py
@@ -4,6 +4,15 @@ from manim_physics import *
 
 class PendulumExample(SpaceScene):
     def construct(self):
+        pends = VGroup(*[Pendulum(i) for i in np.linspace(1,5,7)])
+        self.add(pends)
+        for p in pends:
+            self.make_rigid_body(p.bobs)
+            p.start_swinging()
+        self.wait(10)
+
+class MultiPendulumExample(SpaceScene):
+    def construct(self):
         p = MultiPendulum(RIGHT, LEFT)
         self.add(p)
         self.make_rigid_body(p.bobs)
