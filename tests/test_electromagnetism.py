a5749392ba8b7cba973950ff7c1d527c01e130c4
icedcoffeeee <hazim.saharuddin@gmail.com>
2024-08-01 18:45:43 +0800

    test

--- tests/test_electromagnetism.py
+++ tests/test_electromagnetism.py
@@ -22,3 +22,12 @@ def test_magnetic_field(scene):
     wire = Wire(Circle(2).rotate(PI / 2, UP))
     field = MagneticField(wire)
     scene.add(field, wire)
+
+
+@frames_comparison(base_scene=ThreeDScene)
+def test_magnetic_field_multiple_wires(scene):
+    wire1 = Wire(Circle(2).rotate(PI / 2, RIGHT).shift(UP * 2))
+    wire2 = Wire(Circle(2).rotate(PI / 2, RIGHT).shift(UP * -2))
+    mag_field = MagneticField(wire1, wire2)
+    scene.set_camera_orientation(PI / 3, PI / 4)
+    scene.add(wire1, wire2, mag_field)
